8039ac4cc2c1896fe22c4080c2e2014a44206532
ME7502 <mouadelouarti1@gmail.com>
2026-03-31 16:39:20 +0200

    Initiating the spring project with spring initalizr

---
+++ src/main/java/com/allnight/train_me_api/TrainMeApiApplication.java
@@ -0,0 +1,13 @@
+package com.allnight.train_me_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainMeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainMeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allnight/train_me_api/TrainMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allnight.train_me_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainMeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
